f751fcb72792ce2693bef27f349bfabb1f2004e5
Ruttie <80394237+Ruttie2006@users.noreply.github.com>
2021-08-05 11:34:15 +0200

    rename

---
+++ HowwowKnyight - 1.5/GlobalSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace HowwowKnyight.GlobalSettings
+{
+    public class GlobalSettingsClass
+    {
+        public bool AutoBackup = false;
+
+        public string[] OwO = new[]
+        {
+                " uwu", " owo", " UwU", " OwO", " >w<", " ^w^", " QwQ", " UwU", " @w@", " >.<", " ÕwÕ", "~", "~", "~",
+                "~", "~"
+        };
+    }
+}
